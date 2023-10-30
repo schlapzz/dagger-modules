a381ec204b7222f55408c744b940a9b4c4228479
Christian Schlatter <schlatter@puzzle.ch>
2023-10-30 11:05:10 +0100

    fix for 0.9.1

--- helm-push/main.go
+++ helm-push/main.go
@@ -10,29 +10,7 @@ import (
 type HelmPush struct {
 }
 
-type PushOpts struct {
-	Registry   string
-	Repository string
-	Oci        bool
-	Username   string
-	Password   string
-}
-
-func (p PushOpts) getChartFqdn(name string) string {
-	if p.Oci {
-		return fmt.Sprintf("oci://%s/%s/%s", p.Registry, p.Repository, name)
-	}
-	return fmt.Sprintf("%s/%s/%s", p.Registry, p.Repository, name)
-}
-
-func (p PushOpts) getRepoFqdn() string {
-	if p.Oci {
-		return fmt.Sprintf("oci://%s/%s", p.Registry, p.Repository)
-	}
-	return fmt.Sprintf("%s/%s/%s", p.Registry, p.Repository)
-}
-
-func (h *HelmPush) PackagePush(ctx context.Context, d *Directory, opts PushOpts) error {
+func (h *HelmPush) PackagePush(ctx context.Context, d *Directory, registry string, username string, password string) error {
 	fmt.Fprintf(os.Stdout, "☸️ Helm package and Push")
 	c := dag.Container().From("registry.puzzle.ch/cicd/alpine-base:latest").WithDirectory("/helm", d).WithWorkdir("/helm")
 	version, err := c.WithExec([]string{"sh", "-c", "helm show chart . | yq eval '.version' -"}).Stdout(ctx)
@@ -49,29 +27,7 @@ func (h *HelmPush) PackagePush(ctx context.Context, d *Directory, opts PushOpts)
 
 	name = strings.TrimSpace(name)
 
-	c, err = c.WithExec([]string{"helm", "registry", "login", opts.Registry, "-u", opts.Username, "-p", opts.Password}).Sync(ctx)
-	if err != nil {
-		return err
-	}
-
-	c, err = c.WithExec([]string{"sh", "-c", fmt.Sprintf("helm show chart %s --version %s; echo -n $? > /ec", opts.getChartFqdn(name), version)}).Sync(ctx)
-	if err != nil {
-		return err
-	}
-
-	exc, err := c.File("/ec").Contents(ctx)
-	if err != nil {
-		return err
-	}
-
-	if exc == "0" {
-		//Chart exists
-		return nil
-	}
-
-	fmt.Fprintf(os.Stderr, "chart not exists")
-
-	_, err = c.WithExec([]string{"helm", "package", "."}).WithExec([]string{"sh", "-c", "ls"}).WithExec([]string{"helm", "push", fmt.Sprintf("%s-%s.tgz", name, version), opts.getRepoFqdn()}).Sync(ctx)
+	c, err = c.WithExec([]string{"helm", "registry", "login", registry, "-u", username, "-p", password}).Sync(ctx)
 	if err != nil {
 		return err
 	}
